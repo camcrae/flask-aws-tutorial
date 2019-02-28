ce29578fc231d6899dd36b6bf859f71e3537565d
Cameron Lanata <cam@Camerons-MacBook-Pro.local>
2019-02-28 15:57:33 -0500

    Updated RDS endpoint, again

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://demo:swsquad!@smartaps-customers.c6x5al6bomw6.us-east-2.rds.amazonaws.com:3306/smartaps-customers'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cameron:swsquad!@smartaps-customers.c6x5al6bomw6.us-east-2.rds.amazonaws.com:3306/cameron'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
@@ -10,4 +10,4 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://demo:swsquad!@smartaps-customers.c6x5
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+SECRET_KEY = '0uiSm1zcR5l2ZsozPsz6azfpo7VM9sY2CK1mJzFj'
